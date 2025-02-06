7bcf05ac2557002d111edef6fe083a40e4a4a0f4
Shikhar Gupta <118909293+ShikharrGuptaa@users.noreply.github.com>
2025-02-06 20:38:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Power of 2/power-of-2.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    // Function to check if given number n is a power of two.
+    bool isPowerofTwo(int n) {
+        // code here
+        if(n == 1) return true;
+        
+        int num = 1;
+        while(num < n){
+            num = num << 1;
+            if(num == n) return true;
+        }
+        return false;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+// Driver code
+int main() {
+
+    int t;
+    cin >> t; // testcases
+
+    for (int i = 0; i < t; i++) {
+        int n; // input a number n
+        cin >> n;
+        Solution ob;
+        if (ob.isPowerofTwo(n))
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
+        cout << "~" << endl;
+    }
+
+    return 0;
+}
+// } Driver Code Ends
