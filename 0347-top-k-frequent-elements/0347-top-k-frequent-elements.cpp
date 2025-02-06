50039614e59f8c93c38ca43f14848cf20353cf47
Shikhar Gupta <118909293+ShikharrGuptaa@users.noreply.github.com>
2025-02-06 17:09:38 +0530

    Time: 0 ms (100%), Space: 17.9 MB (72.02%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -7,6 +7,10 @@ public:
         for(int num : nums) 
             mp[num]++;
         
+        // Forming a minHeap that'll store freq of elements along with their number
+        // Goal is to make min heap of most frequencies
+        // Why minHeap? because the least of those freauency already stored in minHeap would be on top and
+        //  if anything comes more than the value stored in top deserves to be in minHeap so we can easily pop it, and can push the new greater value there, keeping the fact together that it needs only of k size so that we get k most frequent elements only
         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
 
         for(auto &entry : mp){
