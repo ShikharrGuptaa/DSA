370d84212cbf281408e256de171db98e0ce63c8f
Shikhar Gupta <118909293+ShikharrGuptaa@users.noreply.github.com>
2025-02-12 16:05:35 +0530

    Time: 5 ms (23.53%), Space: 21.1 MB (18.03%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,29 +1,69 @@
 class Solution {
-private:
-    void dfs(int src, vector<vector<int>>& isConnected,  vector<int> &vis){
+private: 
+    void bfs(int src, vector<vector<int>> &adjList, vector<int> &vis){
+        queue<int> q;// Queue to process the elemnet;
+        q.push(src);
+        vis[src] = 1;
+
+        while(!q.empty()){
+            int Node = q.front();
+            q.pop();
+
+            for(auto adjNode: adjList[Node]){
+                if(!vis[adjNode]){
+                    vis[adjNode] = 1;
+                    q.push(adjNode);
+                }
+            }
+        }
+    }
+
+    void dfs(int src, vector<vector<int>> &adjList, vector<int> &vis){
+        vis[src] = 1;
+
+        for(auto adjNode: adjList[src]){
+            if(!vis[adjNode]){
+                dfs(adjNode, adjList, vis);
+            }
+        }
+    }
+
+    void dfsMatrix(int src, vector<vector<int>>& isConnected, vector<int> &vis){
         vis[src] = 1;
 
         for(int adjNode = 0; adjNode < isConnected.size(); ++adjNode){
             if(isConnected[src][adjNode] == 1 && !vis[adjNode]){
-                dfs(adjNode, isConnected, vis);
+                dfsMatrix(adjNode, isConnected, vis);
             }
         }
     }
 
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int n = isConnected.size();
-        int count = 0;
+        // Vertices
+        int V = isConnected.size();
+        vector<vector<int>> adjList(V);
+        vector<int> vis(V, 0);
 
-        vector<int> vis(n, 0);
-
-        for(int i = 0; i < n; ++i){
-            if(!vis[i]){
-                dfs(i, isConnected, vis);
-                count++;
+        for(int i = 0; i < V; ++i){
+            for(int j = 0; j < V; ++j){
+                if(isConnected[i][j] == 1 && i != j){
+                    adjList[i].push_back(j);
+                    adjList[j].push_back(i);
+                }
             }
         }
 
-        return count;
+        int cnt = 0;
+        for(int i = 0; i < V; ++i){
+            if(!vis[i]){
+                // bfs(i, adjList, vis);
+                // dfs(i, adjList, vis);
+                dfsMatrix(i, isConnected, vis);
+                cnt++;
+            }
+        }
+
+        return cnt;
     }
 };
