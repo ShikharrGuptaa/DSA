a9abcf3f9b40c46bb3360ab35bfe07032f3c614c
Shikhar Gupta <118909293+ShikharrGuptaa@users.noreply.github.com>
2025-02-24 21:11:27 +0530

    Time: 10 ms (48.47%), Space: 17.8 MB (49.76%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,21 +1,60 @@
-class Solution {
+class Solution{
 private:
-    int help(int idx, int buy, int transactions, vector<int>& prices, vector<vector<vector<int>>> &dp){
-        if(idx == prices.size() || transactions == 0) return 0;
+    int help(int idx, int tranNo, int k, vector<int> &prices, vector<vector<int>> &dp){
+        if(idx == prices.size() || tranNo == 2 * k) return 0;
 
-        if(dp[idx][buy][transactions] != -1) return  dp[idx][buy][transactions];
+        if(dp[idx][tranNo] != -1) return dp[idx][tranNo];
 
-        if(buy){
-            return dp[idx][buy][transactions] = max(-prices[idx] + help(idx+1, 0, transactions, prices, dp), help(idx+1, 1, transactions, prices, dp));
+        // Buy
+        if(tranNo % 2 == 0){
+            return dp[idx][tranNo] = max(-prices[idx] + help(idx+1, tranNo+1, k, prices, dp), help(idx+1, tranNo, k, prices, dp));
         }
-        else{
-            return dp[idx][buy][transactions] = max(+prices[idx] + help(idx+1, 1, transactions-1, prices, dp), help(idx+1, 0, transactions, prices, dp));
-        }
+        // Sell
+        return dp[idx][tranNo] = max(+prices[idx] + help(idx+1, tranNo+1, k, prices, dp), help(idx+1, tranNo, k, prices, dp));
     }
-
 public:
-    int maxProfit(int k, vector<int>& prices) {
-        vector<vector<vector<int>>> dp(prices.size(), vector<vector<int>>(2, vector<int>(k+1, -1)));
-        return help(0, 1, k, prices, dp);
+    int maxProfit(int k, vector<int>& prices){
+        vector<vector<int>> dp(prices.size(), vector<int>(2 * k, -1));
+
+        return help(0, 0, k, prices, dp);
+        
     }
 };
+
+
+
+// class Solution {
+// public:
+//     int maxProfit(int k, vector<int>& prices) {
+//         int n = prices.size();
+//         auto dp = vector<vector<pair<int, int>>>(n, vector<pair<int, int>>(k+1, pair<int, int>({0, 0})));
+
+//         for (int i = 0; i <= k; ++i) {
+//             dp[n-1][i] = {0, prices[n-1]};
+//         }
+//         int maxTillNow = prices[n-1];
+//         for (int i = n-2; i >= 0; --i) {
+//             maxTillNow = max(maxTillNow, prices[i]);
+//             dp[i][0] = {0, maxTillNow};
+//         }
+
+
+//         int res = 0;
+//         for (int i = n-2; i >= 0; --i) {
+//             for (int j = 1; j <= k; ++j) {
+//                 int ans = 0;
+//                 ans = max(dp[i+1][j].first, -prices[i] + dp[i+1][j-1].second);
+//                 dp[i][j].first = ans;
+
+//                 ans = 0;
+//                 ans = max(dp[i+1][j].second, prices[i] + dp[i+1][j].first);
+//                 dp[i][j].second = ans;
+
+//                 if (i == 0) {
+//                     res = max(res, dp[i][j].first);
+//                 }
+//             }
+//         }
+//         return res;
+//     }
+// };
